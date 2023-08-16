cbd7cf3149bb83024ba42bb319195c236974afbd
Charle6 <140961714+Charle6@users.noreply.github.com>
2023-08-16 20:22:50 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<?php
+include'index.html';
+include'data.php';
+if(isset($_POST['send'])){
+    $email=$_POST['email'];
+    $mobile=$_POST['subject'];
+    $password=$_POST['message'];
+
+    $sql="insert into `gmail` (name,email,subject,message)
+    values('$name','$email','$subject','$message')";
+    $result=mysqli_query($con,$sql);
+    if($result){          
+    
+    }else{
+        die(mysqli_error($con));
+    }
+
+}
+?>
